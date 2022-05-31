30d1a7ef41af7b9863df6fa4158928065b41051c
Víctor López <vicrenlopez14115@gmal.com>
2022-05-30 19:10:14 -0600

    First commit

---
+++ CalendarioPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace EjemploApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CalendarioPage : Page
+    {
+
+        List<Carro> listaDeCarros = new List<Carro>();
+
+        public CalendarioPage()
+        {
+            this.InitializeComponent();
+        }
+
+        class Carro
+        {
+            public Carro(string marca, string color, string matricula, string dueño)
+            {
+                Marca = marca;
+                Color = color;
+                Matricula = matricula;
+                Dueño = dueño;
+            }
+
+            public Carro(string nombreDueño, string telefonoDueño, string marca, string color, string matricula, string dueño)
+            {
+                NombreDueño = nombreDueño;
+                TelefonoDueño = telefonoDueño;
+                Marca = marca;
+                Color = color;
+                Matricula = matricula;
+                Dueño = dueño;
+            }
+
+            public string NombreDueño { get; set; }
+            public string TelefonoDueño { get; set; }
+            public string Marca { get; set; }
+            public string Color { get; set; }
+
+            public string Matricula { get; set; }
+
+            public string Dueño { get; set; }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var nuevoNombre = name_tb.Text;
+            var nuevoTelefono = name_tb.Text;
+
+            var nuevoCarro = new Carro(nuevoNombre, nuevoTelefono, "Toyota", "Rojo", "012345", "Burgos");
+            listaDeCarros.Add(nuevoCarro);
+
+
+            myDataGrid.ItemsSource = null;
+            myDataGrid.ItemsSource = listaDeCarros;
+           
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace EjemploApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                Console.WriteLine("Botón no funcional");
+            }
+            else
+            {
+                var item = sender.MenuItems.OfType<NavigationViewItem>().First(x => (string)x.Content == (string)args.InvokedItem);
+                NavView_Navigate(item as NavigationViewItem);
+            }
+        }
+
+        private void NavView_Navigate(NavigationViewItem item)
+        {
+            switch (item.Tag)
+            {
+                case "home":
+                    NavView.IsPaneOpen = false;
+                    break;
+                case "pagos":
+                    ContentFrame.Navigate(typeof(Pagos));
+                    break;
+                case "notificaciones":
+                    ContentFrame.Navigate(typeof(Notificaciones));
+                    break;
+                case "calendario":
+                    ContentFrame.Navigate(typeof(CalendarioPage));
+                    break;
+            }
+        }
+    }
+}
